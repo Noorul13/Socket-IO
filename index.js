64727b5facdc85f0e914ddd5074b4c8c8bde3ba2
Noorul Khan <noorul.khan@mobulous.com>
2024-12-19 18:25:01 +0530

    updated

--- index.js
+++ index.js
@@ -11,27 +11,32 @@ const userMap = new Map();
 
 // io.on("connection", (socket) => {
 //     console.log(`A user connected`);
+//     // console.log(socket.id);
 //     const userId = socket.handshake.query.id
+//     // console.log(userId);
 //     userMap.set(userId,socket.id);
 //     console.log("hashmap => ", userMap);
 
 //     socket.on("sendMessage", (msg) => {
 //         console.log(socket.id, msg);
 //         /*
-//         const recieverSocketId = userMap.get(msg.recievedby)
-//         console.log("receiverSocketId",recieverSocketId?.toString())
-//         io.to(recieverSocketId).emit("rcv-msg",{  // Agar specific user ko bhejna ho to
-//             sendBy: socket.handshake.query.id,
-//             msg: msg.msg
-//         })
-//         // io.emit("rcv-msg",msg);  // Agar sab user ko bhejna ho to 
-//         */
+//         // Agar specific user ko bhejna ho to
+//         // const recieverSocketId = userMap.get(msg.recievedby)
+//         // console.log("receiverSocketId",recieverSocketId?.toString())
+//         // io.to(recieverSocketId).emit("rcv-msg",{  
+//         //     sendBy: socket.handshake.query.id,
+//         //     msg: msg.msg
+//         // })
 
-        
-//         // msg.recievedby.forEach(userId => {
-//         //     console.log(`User ID: ${userId}, Socket ID: ${userMap.get(userId.toString())}`);
-//         // });
+//         //io.emit("rcv-msg",msg);  // Agar sab user ko bhejna ho to 
+//        */
 
+//         // agar more than 1 user ko bhejna ho to
+//         msg.recievedby.forEach(userId => {
+//             console.log(`User ID: ${userId}, Socket ID: ${userMap.get(userId.toString())}`);
+//         });
+
+//         /*
 //         // msg.receivedBy should be an array of user IDs
 //         const receiverSocketIds = msg.recievedby.map(userId => userMap.get(userId.toString()))//.filter(id => id); // get socket IDs from userMap
         
@@ -44,59 +49,58 @@ const userMap = new Map();
 //                 msg: msg.msg
 //             });
 //         });
+//         */
 //     });
 //     socket.on("disconnect", () => {
 //         console.log("User disconnected");
 //     });
 // });
 
-
-/*
 // Chat namespace
 
-const chatNamespace = io.of("/chat");
-chatNamespace.on("connection", (socket) => {
-  console.log("Connected to /chat");
-  socket.on("send message", (msg) => {
-    chatNamespace.emit("chat message", msg);
-    console.log(msg);
-  });
-});
+// const chatNamespace = io.of("/chat");
+// chatNamespace.on("connection", (socket) => {
+//   console.log("Connected to /chat");
+//   socket.on("send message", (msg) => {
+//     chatNamespace.emit("chat message", msg);
+//     console.log(msg);
+//   });
+// });
 
-// News namespace
-const newsNamespace = io.of("/news");
-newsNamespace.on("connection", (socket) => {
-  console.log("Connected to /news");
-  socket.on("Send news", (news) => {
-    newsNamespace.emit("news update", news);
-  });
-});
+// // News namespace
+// const newsNamespace = io.of("/news");
+// newsNamespace.on("connection", (socket) => {
+//   console.log("Connected to /news");
+//   socket.on("Send news", (news) => {
+//     newsNamespace.emit("news update", news);
+//   });
+// });
 
-*/
+
 
 // ROOMS
-io.on('connection', (socket) => {
-   console.log('A user connected');
+// io.on('connection', (socket) => {
+//    console.log('A user connected');
 
-   // Join a room
-   socket.on('joinRoom', (room) => {
-       console.log(room);
-       socket.join(room.id);
-       console.log(`User joined room: ${room.id}`);
-   });
+//    // Join a room
+//    socket.on('joinRoom', (room) => {
+//        console.log(room);
+//        socket.join(room.id);
+//        console.log(`User joined room: ${room.id}`);
+//    });
 
-   // Listen for messages in a room
-   socket.on('sendmessage', (data) => {
-      console.log(data);
-       const { room, message } = data;
-       io.to(room).emit('recievemessage', message);
-   });
+//    // Listen for messages in a room
+//    socket.on('sendmessage', (data) => {
+//       console.log(data);
+//        const { room, message } = data;
+//        io.to(room).emit('recievemessage', message);
+//    });
 
-   // Handle user disconnect
-   socket.on('disconnect', () => {
-       console.log('User disconnected');
-   });
-});
+//    // Handle user disconnect
+//    socket.on('disconnect', () => {
+//        console.log('User disconnected');
+//    });
+// });
 
 server.listen(PORT, () => {
     console.log(`Server listen on ${PORT}`);
